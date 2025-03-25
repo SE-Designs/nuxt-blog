c0fdcb34648a31a67bd7ee09820d4c8e8359674e
SE-Designs <dragonlorefnstattrack@gmail.com>
2025-03-25 22:57:51 +0500

    init commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <LayoutNav />
+  </header>
+  <main>
+    <div class="max-w-[1280px] mx-auto px-8">
+      <NuxtPage />
+    </div>
+  </main>
+  <footer>
+    <LayoutFooter />
+  </footer>
+</template>
+<style>
+header {
+  /*  */
+}
+
+main {
+  margin: 60px 0;
+
+  min-height: calc(100dvh - 60px);
+}
+
+footer {
+  height: fit-content;
+}
+</style>
